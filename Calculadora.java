5b831d7a621c59d3076e844310ef69075ff718bf
Eduardo Gomes <egosmail@gmail.com>
2024-03-31 10:37:50 -0400

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    // Método para somar dois números
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    // Método para subtrair dois números
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+}
